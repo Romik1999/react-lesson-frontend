90a9e0a01e260fa25806e768d4708fd234d1e5f2
Roman <usankin.roma@gmail.com>
2023-06-27 00:44:56 +0400

    Delete style css from fileWatcher add get user data from keyboard

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,15 +1,33 @@
 import React, {Fragment} from 'react';
 import {Button, TextField, Typography} from "@mui/material";
 
-const LoginPage = () => {
+const LoginPage = (props: any) => {
+    const {setPassword, setEmail} = props
     return (
         <>
             <Typography variant="h2" fontFamily="Popins" textAlign="center">Авторизация</Typography>
-            <Typography variant="body1" marginBottom={3} fontFamily="Popins" textAlign="center">Введите ваш логин и пароль</Typography>
-            <TextField fullWidth={true} margin="normal" label="Email" variant="outlined" placeholder="Введите ваш email" />
-            <TextField fullWidth={true}  margin="normal" label="Password" variant="outlined" placeholder="Введите ваш пароль" />
-            <Button variant="contained" sx={{fontFamily:"Popins", marginTop: 2, marginBottom: 2, width: "60%" }}>Войти</Button>
-            <Typography variant="body1" sx={{fontFamily:"Popins" }}>У вас нет аккаунта? <span className="incitingText">Регистрация</span></Typography>
+            <Typography variant="body1" marginBottom={3} fontFamily="Popins" textAlign="center">
+                Введите ваш логин и пароль
+            </Typography>
+            <TextField
+                fullWidth={true} margin="normal" label="Email" variant="outlined" placeholder="Введите ваш email"
+                onChange={(e) => {
+                    setEmail(e.target.value)
+                }}/>
+            <TextField
+                fullWidth={true} margin="normal" label="Password" variant="outlined" placeholder="Введите ваш пароль"
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                }}
+            />
+            <Button variant="contained"
+                    type="submit"
+                    sx={{fontFamily: "Popins", marginTop: 2, marginBottom: 2, width: "60%"}}>
+                Войти
+            </Button>
+            <Typography variant="body1" sx={{fontFamily: "Popins"}}>
+                У вас нет аккаунта? <span className="incitingText">Регистрация</span>
+            </Typography>
         </>
     );
 };
