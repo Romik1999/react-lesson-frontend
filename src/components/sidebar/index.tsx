5211271addc90869f061601ee6903b9709b00741
Roman <usankin.roma@gmail.com>
2023-08-01 02:46:26 +0400

    Create sidebar - settings sidebarComponent

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useStyles} from "./styles";
 import {
     Box,
@@ -22,14 +22,56 @@ import {
     SettingsOutlined,
     LogoutOutlined
 } from '@mui/icons-material';
+import {useLocation, useNavigate} from "react-router-dom";
+import FlexBetween from "../flex-between";
 
 const SidebarComponent = (props: any) => {
+    const [active, setActive] = useState('')
     const {isNonMobile, drawerWidth, isOpen, setIsOpen} = props
     const classes = useStyles()
+    const {pathname} = useLocation()
+    const navigate = useNavigate()
+    const theme = useTheme()
+
+    useEffect(() => {
+        setActive(pathname.substring(1))
+    }, [pathname])
+
     return (
-        <div>
-            <h1>This is sidebar</h1>
-        </div>
+        <Box component="nav">
+            {isOpen && (
+                <Drawer
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
+                    variant="persistent"
+                    anchor="left"
+                    sx={{
+                        width: drawerWidth,
+                        '& .MuiDrawer-paper': {
+                            color: theme.palette.secondary.main,
+                            backgroundColor: theme.palette.primary.main,
+                            boxSizing: 'border-box',
+                            width: drawerWidth,
+                        }
+                    }}
+                >
+                    <Box width='100'>
+                        <Box>
+                            <FlexBetween>
+                                <Box display='flex' alignItems="center" gap='10px'>
+                                    <Typography>Demo</Typography>
+                                </Box>
+                                {!isNonMobile && (
+                                    <IconButton onClick={()=>setIsOpen(!isOpen)}>
+                                        <ChevronLeftOutlined />
+                                    </IconButton>
+                                )}
+                            </FlexBetween>
+                        </Box>
+                    </Box>
+                </Drawer>
+            )}
+        </Box>
     );
 };
 
